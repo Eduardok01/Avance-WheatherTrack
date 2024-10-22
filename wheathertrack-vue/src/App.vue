7cd478b1d4f5edf7d90a306a441c57fc12526b27
Ignacio Manríquez <i.manriquez01@ufromail.cl>
2024-10-22 15:26:24 -0300

    Commit. Update
    Add Mapbox dependencies and "MapboxMap.vue" file

--- wheathertrack-vue/src/App.vue
+++ wheathertrack-vue/src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <MapboxMap msg="Mapa"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MapboxMap from './components/MapboxMap.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    MapboxMap
   }
 }
 </script>
---
+++ wheathertrack-vue/src/components/MapboxMap.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="map-container">
+    <div id="map"></div>
+  </div>
+</template>
+  
+<script setup>
+  import { onMounted, ref, onBeforeUnmount } from 'vue'
+  import mapboxgl from 'mapbox-gl'
+  import 'mapbox-gl/dist/mapbox-gl.css'
+  
+  // Reemplazar 'YOUR_MAPBOX_ACCESS_TOKEN' con el token de acceso de Mapbox.
+  // En este caso se ocupa con casos demostrativos el mapa con el siguiente token de acceso.
+  mapboxgl.accessToken = 'pk.eyJ1IjoiYWF2ZW5kYW5vMDQiLCJhIjoiY20yYWxxY2JhMGh2NDJzcHltZzh4ZmJnNyJ9.AilIZoUNRF1i5aLnk5zdpg'
+  
+  const map = ref(null)
+  
+  onMounted(() => {
+    map.value = new mapboxgl.Map({
+      container: 'map', // ID del div donde se monta el mapa
+      style: 'mapbox://styles/mapbox/streets-v11', // Estilo del mapa
+      center: [-72.6173, -38.7490], // Ubicación Universidad de La Frontera. Temuco, Chile,
+      zoom: 15 // Nivel de zoom inicial
+    })
+  
+    // Agrega un marcador de ejemplo (Este caso ubicado en la UFRO)
+    new mapboxgl.Marker()
+      .setLngLat([-72.6173, -38.7490])
+      .setPopup(new mapboxgl.Popup().setText('A sample marker.'))
+      .addTo(map.value)
+  })
+  
+  onBeforeUnmount(() => {
+    // Destruye el mapa para liberar recursos
+    if (map.value) {
+      map.value.remove()
+    }
+  })
+  </script>
+  
+  <style scoped>
+  .map-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    width: 100%;
+  }
+  #map {
+    height: 450px;
+    width: 90%;
+    max-width: 1000px;
+  }
+  </style>
