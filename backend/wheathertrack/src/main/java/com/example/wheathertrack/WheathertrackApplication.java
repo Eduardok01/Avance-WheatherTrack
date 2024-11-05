2c0fa91bba5a1319ac7bc7835e3557852b3c8768
Ignacio Manríquez <i.manriquez01@ufromail.cl>
2024-11-05 15:14:45 -0300

    Commit: Upload Backend folder structure
    Upload backend folder using Springboot + upload files

---
+++ backend/wheathertrack/src/main/java/com/example/wheathertrack/WheathertrackApplication.java
@@ -0,0 +1,13 @@
+package com.example.wheathertrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WheathertrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WheathertrackApplication.class, args);
+	}
+
+}
---
+++ backend/wheathertrack/src/test/java/com/example/wheathertrack/WheathertrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wheathertrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WheathertrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
